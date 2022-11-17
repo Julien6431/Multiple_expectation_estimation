79641f754d28afe3254004021d038b05087954b2
Julien6431 <julien.demange-chryst@onera.fr>
2022-11-17 17:54:33 +0100

    clean expetations and moments

--- numerical_tests/Gaussian_moments.py
+++ numerical_tests/Gaussian_moments.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Nov  9 15:31:11 2022
 
-@author: jdemange
+@author: Julien Demange-Chryst
 """
 
 
@@ -18,7 +18,7 @@ from adaptative_CV import multiple_expectations_cv
 
 from tqdm import tqdm
 
-#%% Settting
+#%% Settting and reference values
 
 input_distr = ot.Normal(1)
 J = 10
@@ -30,7 +30,6 @@ def phi(x):
 
 ot_phi = ot.PythonFunction(1,J,phi)
 
-#%%
 
 ref_values = np.zeros(J)
 for i in tqdm(range(1,J+1)):
@@ -38,50 +37,32 @@ for i in tqdm(range(1,J+1)):
    
     
    
-#%%
+#%% Execution of the algorithm
 
-n_rep = 200#2*10**2
+n_rep = 2*10**2
 MC = np.zeros((n_rep,J))
-CV = np.zeros((n_rep,J))
+ISCV = np.zeros((n_rep,J))
 
 N = 2*10**4
 
 weights = np.ones(J)/ref_values**2
+
 for n in tqdm(range(n_rep)):
     X = np.array(input_distr.getSample(N))
     for j in range(1,J+1):
         MC[n,j-1] = np.mean(X**(2*j))
-    
-    CV[n],_,_,_ = multiple_expectations_cv(ot_phi,weights,input_distr,N_max=N,cross_entropy="SG")
+    ISCV[n],_,_,_ = multiple_expectations_cv(ot_phi,weights,input_distr,N_max=N,cross_entropy="SG")
 
 print("\nVariances : \n")
 print(f"MC : {np.sum(weights*np.var(MC,axis=0))}")
-print(f"CV : {np.sum(weights*np.var(CV,axis=0))}")
+print(f"ISCV : {np.sum(weights*np.var(ISCV,axis=0))}")
 
 #%% Save data
 
 np.savez("data/Gaussian_moment_10.npz",
          ref_values=ref_values,
          MC=MC,
-         CV=CV)
-
-#%%
-
-fig,ax = plt.subplots(1,5,figsize=(25,15))
-for j in range(5):
-    ax[j].boxplot(ref_values[j].reshape((-1,1)),sym='k+',positions=[0])
-    ax[j].boxplot(MC[:,j],sym='k+',positions=[.5])
-    ax[j].boxplot(CV[:,j],sym='k+',positions=[1])
-    
-#%%
-
-fig,ax = plt.subplots(2,5,figsize=(25,15))
-for i in range(2):
-    for j in range(5):
-        ax[i,j].boxplot(ref_values[5*i+j].reshape((-1,1)),sym='k+',positions=[0])
-        ax[i,j].boxplot(MC[:,5*i+j],sym='k+',positions=[.5])
-        ax[i,j].boxplot(CV[:,5*i+j],sym='k+',positions=[1])
-
+         ISCV=ISCV)
 
 #%%
 
--- numerical_tests/cantilever_beam_expectations.py
+++ numerical_tests/cantilever_beam_expectations.py
@@ -2,29 +2,27 @@
 """
 Created on Mon Aug  1 13:36:05 2022
 
-@author: jdemange
+@author: Julien Demange-Chryst
 """
 
 #%% Modules
 
 import numpy as np
 import openturns as ot
-import matplotlib.pyplot as plt
+from tqdm import tqdm
 from cantilver_beam_model import fleche
 
 import sys
 sys.path.append("../src")
 from adaptative_CV import multiple_expectations_cv
 
-from tqdm import tqdm
 
-#%% Input
+#%% Input distribution
 
 def get_input_distr(parameters):
 
-    delta1,delta2,delta3,delta4,delta5,delta6,delta7,delta8,delta9 = parameters    
-
     dim = 6
+    delta1,delta2,delta3,delta4,delta5,delta6,delta7,delta8,delta9 = parameters    
     
     mu_FX,sigma_FX,_ = ot.LogNormalMuSigmaOverMu(delta1,0.08).evaluate()
     distr_FX = ot.LogNormal(mu_FX,sigma_FX)
@@ -38,95 +36,85 @@ def get_input_distr(parameters):
     distr_L = ot.Normal(delta6,0.1*delta6)
     
     corr_matrix = ot.CorrelationMatrix(dim)
-    corr_matrix[3,4] = delta7#-0.55
-    corr_matrix[3,5] = delta8#0.45
-    corr_matrix[4,5] = delta9#0.45
+    corr_matrix[3,4] = delta7
+    corr_matrix[3,5] = delta8
+    corr_matrix[4,5] = delta9
         
     Copule_normale = ot.NormalCopula(corr_matrix)
     input_distr = ot.ComposedDistribution([distr_FX,distr_FY,distr_E,distr_w,distr_t,distr_L],Copule_normale)
 
     return input_distr
 
-#%%
+#%% Marginal distributions of the parameters
 
-distr_param1 = ot.Uniform(525,575)#525,575
-distr_param2 = ot.Uniform(425,475)#425,475
-distr_param3 = ot.Uniform(175,225)#175.225
+distr_param1 = ot.Uniform(525,575)
+distr_param2 = ot.Uniform(425,475)
+distr_param3 = ot.Uniform(175,225)
 distr_param4 = ot.Uniform(0.06,0.07)
 distr_param5 = ot.Uniform(0.09,0.1)
-distr_param6 = ot.Uniform(3,6)#4,5
-distr_param7 = ot.Uniform(-0.6,0)#-.6,0
-distr_param8 = ot.Uniform(0,0.5)#0,0.5
-distr_param9 = ot.Uniform(0,0.5)#0,0.5
+distr_param6 = ot.Uniform(4,5)
+distr_param7 = ot.Uniform(-0.6,0)
+distr_param8 = ot.Uniform(0,0.5)
+distr_param9 = ot.Uniform(0,0.5)
 
 distr_params = ot.ComposedDistribution([distr_param1,distr_param2,distr_param3,distr_param4,distr_param5,distr_param6,distr_param7,distr_param8,distr_param9])
 
-#%%
+#%% Generation of a set of parameters
 
-J = 10**2#1.5*10**1
+J = 10**2
 sample_params = np.array(ot.LHSExperiment(distr_params, J).generate())
-#sample_params = np.array(distr_params.getSample(J))
 input_distrs = [get_input_distr(params) for params in sample_params]
 
 phi = ot.PythonFunction(6,1,fleche)
 
-#%%
-
-ref_values = np.zeros(J)
-for i in tqdm(range(J)):
-    d = input_distrs[i]
-    X = d.getSample(10**6)
-    Y = np.array(phi(X))
-    ref_values[i] = np.mean(Y)
-    
-
-#%%
+#%% Execution of the algorithm
 
 n_rep = 2*10**2
-MC = np.zeros((n_rep,J))
-CV = np.zeros((n_rep,J))
+MC_individual = np.zeros((n_rep,J))
+MC_mixture = np.zeros((n_rep,J))
+ISCV = np.zeros((n_rep,J))
 
 N = 2*10**4
 
 mc_distr = ot.Mixture(input_distrs)
-def hsd(x):
+def get_PDFs(x):
     return [d.computePDF(x) for d in input_distrs]
-compute_input_PDF = ot.PythonFunction(6,J,hsd)
+compute_input_PDF = ot.PythonFunction(6,J,get_PDFs)
 
-weights = np.ones(J)#/ref_values**2
+weights = np.ones(J)
 
 for n in tqdm(range(n_rep)):
+    #MC individual
+    Nj = N//J
+    for j in range(J):
+        Xj = input_distrs[j].getSample(Nj)
+        Yj = np.array(phi(Xj))
+        MC_individual[n,j] = np.mean(Yj)
+    
+    #MC mixture
     X = mc_distr.getSample(N)
     f_X = np.array(compute_input_PDF(X))
     h_X = np.array(mc_distr.computePDF(X)) 
     W = f_X/h_X
     Y = np.array(phi(X))
-    MC[n] = np.mean(Y*W,axis=0)
-    CV[n],_,_ = multiple_expectations_cv(phi,weights,input_distrs,N_max=N,cross_entropy="SG")
-
+    MC_mixture[n] = np.mean(Y*W,axis=0)
+    
+    #Control variates
+    ISCV[n],_,_,_ = multiple_expectations_cv(phi,weights,input_distrs,N_max=N,cross_entropy="SG")
+    
 print("\nVariances : \n")
-print(f"MC : {np.sum(weights*np.var(MC,axis=0))}")
-print(f"CV : {np.sum(weights*np.var(CV,axis=0))}")
+print(f"MC : {np.sum(weights*np.var(MC_individual,axis=0))}")
+print(f"CV : {np.sum(weights*np.var(ISCV,axis=0))}")
 
 
-#%%
-
-fig,ax = plt.subplots(2,5,figsize=(25,15))
-for i in range(2):
-    for j in range(5):
-        ax[i,j].boxplot(ref_values[5*i+j].reshape((-1,1)),sym='k+',positions=[0])
-        ax[i,j].boxplot(MC[:,5*i+j],sym='k+',positions=[.5])
-        ax[i,j].boxplot(CV[:,5*i+j],sym='k+',positions=[1])
         
-#%%
+#%% Save data
 
-fig,ax = plt.subplots(10,10,figsize=(25,25))
-for i in range(10):
-    for j in range(10):
-        ax[i,j].boxplot(ref_values[10*i+j].reshape((-1,1)),sym='k+',positions=[0])
-        ax[i,j].boxplot(MC[:,10*i+j],sym='k+',positions=[.5])
-        ax[i,j].boxplot(CV[:,10*i+j],sym='k+',positions=[1])
-        
-#%%
+ref_values = np.load("data/Cantilever_beam_expectations.npz")['ref_values']
 
-np.savez("data/Cantilever_beam_expectations_36.npz",ref_values=ref_values,MC=MC,CV=CV,distr_params=sample_params)
+np.savez("data/Cantilever_beam_expectations.npz",
+         ref_values=ref_values,
+         MC_ind=MC_individual,
+         MC_mixt=MC_mixture,
+         ISCV=ISCV,
+         distr_params=sample_params)
